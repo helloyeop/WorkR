4b8ef17dba213a5f30faa5eed6d775a5c7d04e18
helloyeop <58205527+helloyeop@users.noreply.github.com>
2019-12-04 21:49:49 +0900

    doing

--- 실습_문제/HyeonDongyeop_20191205.R
+++ 실습_문제/HyeonDongyeop_20191205.R
@@ -1,6 +1,6 @@
 #
-# í˜„ë™ì—½
-# ìž‘ì„±ì¼ : 2019.12.04 //ì œì¶œì¼ : 2019.12.05
+# ?˜„?™?—½
+# ?ž‘?„±?¼ : 2019.12.04 //? œì¶œì¼ : 2019.12.05
 #
 
 #ë¬¸ì œ1)
@@ -32,13 +32,14 @@ st
 #Q.1
 boxplot(st)
 
-#Q.2
+#Q.2 --------------
+install.packages("dplyr")
 library(dplyr)
 st %>% filter(population)
 
-
-
-#Q.3
+.libPaths("C:/R/library")
+.libPaths()
+#Q.3 ------------------
 
 
 # ë¬¸ì œ3)
@@ -57,9 +58,86 @@ apply(AQ,1,col_na)
 #Q.4
 AQ[!complete.cases(AQ),]
 
-#Q.5
-AQ[!complete.cases(AQ),]
+#Q.5 --------------
 
 
+#AQ[is.na(AQ)] <- colMeans(AQ,na.rm=T)
+for( i in 1:ncol(AQ)){
+  AQ[is.na(AQ)[,i],][,i] <- mean(AQ[,i],na.rm=T)
+  
+}
+AQ
+AQ2 <- AQ
+AQ2
+
+
+
+
+# ¹®Á¦4)
+
+# Q.1
+library(dplyr)
+st <- as.data.frame(state.x77)
+st %>% arrange(Population)
+
+#Q.2
+st %>% arrange(desc(Income))
+
+#Q.3
+dt<- st %>% arrange(Illiteracy) %>% 
+  head(10)
+st %>% group_by(rownames(st)) %>% 
+  arrange(Illiteracy)
+
+# ¹®Á¦5)
+
+#Q.1
+mt.gear <- split(mtcars, mtcars$gear)
+mt.gear
+
+
+#Q.2
+mt.gear$`4`
+
+
+#Q.3
+mt.gear.35 <- merge(mt.gear$`3`,mt.gear$`5`, all = T)
+mt.gear.35
+
+
+
+#Q.4
+subset(mtcars, wt<3.0 & wt > 1.5)
+
+# ¹®Á¦6)
+
+#Q.1
+authors <- data.frame( surname = c( "Twein", "Venables", "Tierney", "Ripley", "McNeil" ),
+                       nationality = c( "US", "Australia", "US", "UK", "Australia" ),
+                       retired = c( "yes", rep( "no", 4 ) ) )
+books <- data.frame( name = c( "Johns", "Venables", "Tierney", "Ripley", "Ripley", "McNeil" ),
+                     title = c( "Exploratory Data Analysis", 
+                                "Modern Applied Statistics ...",
+                                "LISP-STAT",
+                                "Spatial Statistics", "Stochastic Simulation",
+                                "Interactive Data Analysis" ),
+                     other.author = c( NA, "Ripley", NA, NA, NA, NA ) )
+
+authors
+books
+merge(authors,books,by.x = "surname",
+      by.y = "name")
+
+#Q.2
+merge(authors,books,by.x = "surname",
+      by.y = "name",all.x = T)
+#Q.3
+merge(authors,books,by.x = "surname",
+      by.y = "name",all.y  = T)
+
+
+#Q.4
+merge(authors,books,by.x = "surname",
+      by.y = "other.author")
 
 
